8e08f24d6943f0ad164aa14b8bb86beac60de49a
Muhammed Kocabas <muhammed.kocabas@metu.edu.tr>
2017-09-27 16:21:27 +0300

    add h5 to json converter, add json results

---
+++ h52json.py
@@ -0,0 +1,39 @@
+import h5py
+import json
+import sys
+
+# read hdf5 file
+h5_file = sys.argv[1]
+f = h5py.File(h5_file, 'r')
+key = f.keys()[0]
+data = list(f[key])
+data = data[:-1]
+
+# read text file
+txt_file = sys.argv[2]
+with open(txt_file, 'r') as f:
+    X = [tuple(i.split(' ')) for i in f]
+    X = zip(*X)
+    names = X[0]
+    ids = map(lambda x: int(x[:-4]), names)
+    scores = map(float, X[1])
+    det = zip(ids,scores)
+keypoints = zip(det, data)
+
+# combine them
+result = []
+ann = dict.fromkeys(['image_id', 'category_id', 'keypoints', 'score'])
+for k in keypoints:
+    ann['image_id'] = k[0][0]
+    ann['score'] = k[0][1]
+    ann['category_id'] = 1
+    ks = []
+    for i in k[1]:
+        ks.append(i[0])
+        ks.append(i[1])
+        ks.append(1)
+    ann['keypoints'] = ks
+    result.append(ann)
+# save json
+with open('result_%s.json'%txt_file[10:-3], 'w') as f:
+    json.dump(result, f)
